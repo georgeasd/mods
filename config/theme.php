15f00e96c5044a3ee91f9cde09684431d8114f91
Sunel Tr <sunelbe@gmail.com>
2016-11-12 23:46:27 +0530

    added bundling in compilation step

--- config/theme.php
+++ config/theme.php
@@ -8,10 +8,12 @@ return  [
 	'compliers' => [
 		'js' => [
 			Mods\Theme\Compiler\Script\Minifier::class,
+			Mods\Theme\Compiler\Script\Bundle::class,
 			Mods\Theme\Compiler\Script\Move::class
 		],
 		'css' => [
 			Mods\Theme\Compiler\Style\Minifier::class,
+			Mods\Theme\Compiler\Style\Bundle::class,
 			Mods\Theme\Compiler\Style\Move::class
 		],
 	],
