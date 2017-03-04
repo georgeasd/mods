32d908b2312f063ae1d2d01368bca40b1921da06
Sunel Tr <sunelbe@gmail.com>
2017-03-04 19:36:02 +0530

    Added img compiles to theme

--- config/theme.php
+++ config/theme.php
@@ -2,21 +2,25 @@
 
 return  [
 	'asset' => [
-		'css',
-		'js',
-	],
-	'compliers' => [
-		'js' => [
-			Mods\Theme\Compiler\Script\Minifier::class,
-			Mods\Theme\Compiler\Script\Bundle::class,
-			Mods\Theme\Compiler\Script\Move::class
-		],
-		'css' => [
-			Mods\Theme\Compiler\Style\Minifier::class,
-			Mods\Theme\Compiler\Style\Bundle::class,
-			Mods\Theme\Compiler\Style\Move::class
-		],
-	],
+        'css',
+        'js',
+        'img',
+    ],
+    'compliers' => [
+        'js' => [
+            Mods\Theme\Compiler\Script\Minifier::class,
+            Mods\Theme\Compiler\Script\Bundle::class,
+            Mods\Theme\Compiler\Script\Move::class
+        ],
+        'css' => [
+            Mods\Theme\Compiler\Style\Minifier::class,
+            Mods\Theme\Compiler\Style\Bundle::class,
+            Mods\Theme\Compiler\Style\Move::class
+        ],
+        'img' => [
+            Mods\Theme\Compiler\File\Move::class
+        ],
+    ],
 	'frontend' => [
 		'active' => env('DEFAULT_FRONT_THEME','default'),
 	],
